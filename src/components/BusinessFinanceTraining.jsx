45ec360025d6d027c601a6a290f2f3744eeac32b
shamsiddin <devshamsiddin@gmain.com>
2025-01-04 23:16:59 +0500

    add skleton

--- src/components/BusinessFinanceTraining.jsx
+++ src/components/BusinessFinanceTraining.jsx
@@ -1,11 +1,12 @@
-
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 
 const BusinessFinanceTraining = () => {
-  const { t } = useTranslation();
+  const {t} = useTranslation();
   return (
     // Business and Finance Training Section
-    <div id="about" className="py-20">
+    <div
+      id="about"
+      className="py-20">
       <div className="lg:pt-10 pb-24">
         <div className="container mx-auto max-w-7xl px-5 ">
           <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center  lg:gap-6 gap-10  ">
@@ -21,7 +22,7 @@ const BusinessFinanceTraining = () => {
 
             {/* Right Section (Image) */}
             <div className=" flex md:justify-end ">
-              <div className=" flex items-center justify-center w-full h-60 bg-gray-300 rounded sm:w-96 ">
+              <div className=" skeleton  flex items-center justify-center w-full h-60 bg-gray-300 rounded sm:w-96 ">
                 <svg
                   className="w-10 h-10 text-gray-200 "
                   aria-hidden="true"
@@ -79,7 +80,7 @@ const BusinessFinanceTraining = () => {
             <div className="  ">
               <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4">
                 {" "}
-               {t("understanding_finance.title")}
+                {t("understanding_finance.title")}
               </h1>
               <p className="text-md lg:text-lg text-gray-700">
                 {t("understanding_finance.info")}
@@ -88,7 +89,7 @@ const BusinessFinanceTraining = () => {
 
             {/* Right Section (Image) */}
             <div className=" flex md:justify-end">
-            <div className=" skeleton flex items-center justify-center w-full h-60 bg-gray-300 rounded sm:w-96 ">
+              <div className=" skeleton flex items-center justify-center w-full h-60 bg-gray-300 rounded sm:w-96 ">
                 <svg
                   className="w-10 h-10 text-gray-200 "
                   aria-hidden="true"
@@ -112,10 +113,15 @@ const BusinessFinanceTraining = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center  lg:gap-6 gap-10  ">
             {/* Right Section (Image) */}
             <div className="order-2 md:order-1 flex justify-start">
-            <div className=" skeleton flex items-center justify-center w-full h-60 bg-gray-300 rounded sm:w-96 ">
-            <svg className="w-12 h-12 text-gray-200  me-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-        </svg>
+              <div className=" skeleton flex items-center justify-center w-full h-60 bg-gray-300 rounded sm:w-96 ">
+                <svg
+                  className="w-12 h-12 text-gray-200  me-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20">
+                  <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                </svg>
               </div>
             </div>
 
@@ -126,7 +132,7 @@ const BusinessFinanceTraining = () => {
               </h4>
               <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4">
                 {" "}
-               {t("ceo_message.title")}
+                {t("ceo_message.title")}
               </h1>
               <p className="text-md lg:text-lg text-gray-700">
                 {t("ceo_message.info")}
