f52c8e216c733898441d768d1afb4a54a3160d79
shamsiddin <devshamsiddin@gmain.com>
2025-01-09 03:54:41 +0500

    adda n=another url

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,28 +1,15 @@
-
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from "react";
 
 const FetchSwaggerData = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://mission.uz/en/api/v1/appeal-categories/')
-      .then(response => response.json())
-      .then(json => setData(json));
+    fetch("https://petstore.swagger.io/v2/pet/1")
+      .then((response) => response.json())
+      .then((json) => setData(json));
   }, []);
 
-  return (
-    <div>
-      {data.length > 0 ? (
-        <ul>
-          {data.map((item, index) => (
-            <li key={index}>{item.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
-  );
+  return <div>{data ? <code>{JSON.stringify(data)}</code> : <div>Loading...</div>}</div>;
 };
 
 export default FetchSwaggerData;
