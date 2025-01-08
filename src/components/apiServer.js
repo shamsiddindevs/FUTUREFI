dfcf98a76294098139caddaf88f22501a30ed5d2
shamsiddin <devshamsiddin@gmain.com>
2025-01-09 04:07:13 +0500

    remove vite.json part

--- src/components/apiServer.js
+++ src/components/apiServer.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
+
 console.log(BASE_URL);
 
 
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    __APP_ENV__: import.meta.env.VITE_VERCEL_ENV,
-  },
+
   plugins: [react()],
-    
+
 })
